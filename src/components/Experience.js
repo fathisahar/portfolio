59d4173cc693e02cd1441620d45a4dfce016d5b9
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-08-29 23:28:26 -0400

    job description +  project icons

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,14 +10,22 @@ function Experience() {
         <p className='date'>May 2024 to August 2024</p>
           <h3 className='title'>Software Developer Intern</h3>
           <p className='company'>Nokia</p>
-          <p className='description'>yada yada</p>
-          
+          <p className='description'>
+          • Automated the installation of Rocky Linux 9 on 10+ HP-iLO servers using <span className='semibold'>Ansible</span>, streamlining deployment processes. <br/>
+          • Migrated SysPass database from an unsupported VM OS to a new host, utilizing <span className='semibold'>MariaDB</span>, <span className='semibold'>PHP</span>, and <span className='semibold'>Apache</span> with HTTP configurations, ensuring compatibility and improved performance. <br/>
+          • Developed a <span className='semibold'>Python</span>-based booking system for Nokia's customer support team, managing over 100 nodes with automated <span className='semibold'>SSH connections</span> for information retrieval and <span className='semibold'>HTML webpage</span> generation. <br/>
+          • Developed scripts for comprehensive testing and validation of Nokia 7210 SAS-Mxp network switch.
+        </p>
+
         </div>
         <div className="experience-item">
           <p className='date'>May 2023 to September 2023</p>
           <h3 className='title'>Product Owner Intern</h3>
-          <p className='company'>Dell Technologies </p>
-          <p className='description'>yada yada</p>
+          <p className='company'>Dell Technologies</p>
+          <p className='description'>
+          • Co-managed three development teams, applying <span className='semibold'>Agile</span> methodologies in scrum meetings and sprint planning sessions for two cloud orchestration platforms which leveraged technologies such as <span className='semibold'>Apache Kafka</span>, <span className='semibold'>Kubernetes</span>, <span className='semibold'>RedHat Ansible</span>, and <span className='semibold'>Docker</span>. <br/>
+          • Collaborated with four product owners to analyze technical data, emphasizing variances in predicted and actual sizing through precise measurement of 11 features' story points in <span className='semibold'>Jira</span> for two cloud platform orchestration products. 
+          </p>
         </div>
       </section>
     </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import './Projects.css';
+import MapIcon from '@mui/icons-material/Map';
+import KitchenIcon from '@mui/icons-material/Kitchen';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 function Projects() {
-
-
   return (
     <div>
       <h2>Projects</h2>
       <section className="projects">
       <div className="project-item">
-        <h3>QuestBit</h3>
-        <p>Gamified task management application where users compete for a spot on the leaderboard with friends to complete Quests and their QuestBits.</p>
+        <h3 className='title'>QuestBit</h3>
+        <MapIcon />
+        <p className='description'>Gamified task management application where users compete for a spot on the leaderboard with friends to complete Quests and their QuestBits.</p>
         <p>Made with TypeScript, React-Native, Appwrite.</p>
       </div>
 
       <div className="project-item">
-        <h3>KitchenHelper</h3>
-        <p>Web application to keep track of user's ingredients and recipes, offering recipe based on inventory. </p>
+        <h3 className='title'>KitchenHelper</h3>
+        <KitchenIcon />
+        <p className='description'>Web application to keep track of user's ingredients and recipes, offering recipe based on inventory. </p>
         <p> Made with React, JavaScript, Flask, Python, MySQL</p>
       </div>
 
       <div className="project-item">
-        <h3>FitHub</h3>
-        <p>Sport center management web application to allow owners, instructors and users to organize courses.</p>
-        <p>Made with<span className="tag">Vue.js</span> , JavaScript, PostgreSQL, Spring</p>
+        <h3 className='title'>FitHub</h3>
+        <FitnessCenterIcon/>
+        <p className='description'>Sport center management web application to allow owners, instructors and users to organize courses.</p>
+        <p>Made with Vue.js, JavaScript, PostgreSQL, Spring</p>
       </div>
     </section>
     </div>
