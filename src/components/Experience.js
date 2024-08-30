e234caf2be01a2cddf01a58114ad1e4fb31852fe
fathisahar <sahar.fathi@mail.mcgill.ca>
2024-08-30 09:44:12 -0400

    adding a bit of color

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,8 +8,8 @@ function Experience() {
       <section className="experience">
         <div className="experience-item">
         <p className='date'>May 2024 to August 2024</p>
-          <h3 className='title'>Software Developer Intern</h3>
-          <p className='company'>Nokia</p>
+          <h3 className='nokia-title'>Software Developer Intern</h3>
+          <img className='nokia' src={require('./nokia.png')}/>
           <p className='description'>
           • Automated the installation of Rocky Linux 9 on 10+ HP-iLO servers using <span className='semibold'>Ansible</span>, streamlining deployment processes. <br/>
           • Migrated SysPass database from an unsupported VM OS to a new host, utilizing <span className='semibold'>MariaDB</span>, <span className='semibold'>PHP</span>, and <span className='semibold'>Apache</span> with HTTP configurations, ensuring compatibility and improved performance. <br/>
@@ -20,8 +20,8 @@ function Experience() {
         </div>
         <div className="experience-item">
           <p className='date'>May 2023 to September 2023</p>
-          <h3 className='title'>Product Owner Intern</h3>
-          <p className='company'>Dell Technologies</p>
+          <h3 className='dell-title'>Product Owner Intern</h3>
+          <img className='dell' src={require('./dell.png')}/>
           <p className='description'>
           • Co-managed three development teams, applying <span className='semibold'>Agile</span> methodologies in scrum meetings and sprint planning sessions for two cloud orchestration platforms which leveraged technologies such as <span className='semibold'>Apache Kafka</span>, <span className='semibold'>Kubernetes</span>, <span className='semibold'>RedHat Ansible</span>, and <span className='semibold'>Docker</span>. <br/>
           • Collaborated with four product owners to analyze technical data, emphasizing variances in predicted and actual sizing through precise measurement of 11 features' story points in <span className='semibold'>Jira</span> for two cloud platform orchestration products. 
